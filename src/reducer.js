d026c31f4f6bc0f371a13a9ccfed6f79c5c76e37
Karolin APS <porrask92@gmail.com>
2021-03-24 12:07:04 -0500

    ajustes by eslint

--- src/reducer.js
+++ src/reducer.js
@@ -1,23 +1,28 @@
 export default function reducer(state, action) {
   switch (action.type) {
-    case "CHARACTERS_LIST":
+    case "CHARACTERS_LIST": {
       return { ...state, charactersList: action.payload };
-    case "FILTER_BY_NAME":
+    }
+    case "FILTER_BY_NAME": {
       const filterByName = state.charactersList.filter((el) =>
         el.name.toLowerCase().includes(action.payload.toLowerCase())
       );
       return { ...state, filterByName };
-    case "FILTER_BY_STATUS":
+    }
+    case "FILTER_BY_STATUS": {
       const filterByStatus = state.charactersList.filter((el) =>
         el.status.toLowerCase().includes(action.payload.toLowerCase())
       );
       return { ...state, filterByStatus };
-    case "FILTER_BY_GENRE":
+    }
+    case "FILTER_BY_GENRE": {
       const filterByGenre = state.charactersList.filter((el) =>
         el.genre.toLowerCase().includes(action.payload.toLowerCase())
       );
       return { ...state, filterByGenre };
-    default:
+    }
+    default: {
       return state;
+    }
   }
 }
